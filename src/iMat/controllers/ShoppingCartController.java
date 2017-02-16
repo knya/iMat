2856ef80470d48014d95ab4fe5e61bb26a79945b
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-02-16 23:56:12 +0100

    added ShoppingCartController + fxml

---
+++ src/iMat/controllers/ShoppingCartController.java
@@ -0,0 +1,17 @@
+package iMat.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by JOHAN on 2017-02-16.
+ */
+public class ShoppingCartController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
