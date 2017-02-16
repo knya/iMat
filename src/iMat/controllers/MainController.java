815ce185a40b50e9e5e6688d264cc009bd156b33
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-02-16 23:41:09 +0100

    MainController doesnt seem to work for now, changing to separate initializable controllers until fix

--- src/iMat/controllers/MainController.java
+++ src/iMat/controllers/MainController.java
@@ -2,16 +2,16 @@ package iMat.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    @FXML private SearchController searchController;
+//    private SearchController searchController;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        searchController.init(this);
+//        searchController.init(this);
+
     }
 }
--- src/iMat/controllers/SearchController.java
+++ src/iMat/controllers/SearchController.java
@@ -1,20 +1,47 @@
 package iMat.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
- * Created by Johan on 2017-02-14.
+ * Search controller for search function.
  */
-public class SearchController {
+public class SearchController implements Initializable {
 
-    private MainController mainController;
+    private IMatDataHandler dataHandler;
+    private String searchText;
 
     @FXML private TextField searchTextField;
     @FXML private Button searchButton;
 
-    public void init(MainController mainController) {
-        this.mainController = mainController;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        dataHandler = IMatDataHandler.getInstance();
+        addSearchInputListener();
+    }
+
+    private void addSearchInputListener() {
+        searchTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue != null && !newValue.isEmpty()) {
+                    searchText = newValue;
+                }
+            }
+        });
+    }
+
+    @FXML
+    private void searchButtonActionPerformed(ActionEvent event){
+        dataHandler.findProducts(searchText);
     }
 }
