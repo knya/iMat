afe11246eb56d25621a2ba51a0d20f8bef96029d
knya <johsvenn@student.chalmers.se>
2017-02-25 16:04:48 +0100

    removed some comments

--- src/iMat/controllers/cells/ProductCategoryCellController.java
+++ src/iMat/controllers/cells/ProductCategoryCellController.java
@@ -19,25 +19,16 @@ public class ProductCategoryCellController extends AbstractCellController {
     @FXML private AnchorPane productCategoryCellPane;
     @FXML private Label nameLabel;
 
-//    private IMatDataHandler dataHandler;
-
     private ProductCategory productCategory;
-
-//    @Override
-//    public void initialize(URL location, ResourceBundle resources) {
-//        dataHandler = IMatDataHandler.getInstance();
-//    }
 
     public AnchorPane getAnchorPane() {
         return productCategoryCellPane;
     }
 
-    @Override
     public void setLabels() {
         nameLabel.setText(String.valueOf(productCategory));
     }
 
-    @Override
     public void inject(ProductCategory productCategory) {
         this.productCategory = productCategory;
     }
