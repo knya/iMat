3102f68598b07ee4cf623eb8d9cafb59cc85f3fc
knya <johsvenn@student.chalmers.se>
2017-02-28 12:51:14 +0100

    added FavoriteProductCell and FavoriteProductCellController, for use in Favorites tab.

---
+++ src/iMat/controllers/cells/FavoriteProductCellController.java
@@ -0,0 +1,19 @@
+package iMat.controllers.cells;
+
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * Created by Johan on 2017-02-28.
+ */
+public class FavoriteProductCellController extends AbstractCellController {
+
+    @Override
+    public AnchorPane getAnchorPane() {
+        return null;
+    }
+
+    @Override
+    public void setLabels() {
+
+    }
+}
