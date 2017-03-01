3f164c4b58cf6777f9f607eb111aa16c2dcdaa8e
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-03-01 23:09:12 +0100

    ugly fix for adding labels to corresponding ProdutCategory, but don't know a better way right now

--- src/iMat/controllers/cells/ProductCategoryCellController.java
+++ src/iMat/controllers/cells/ProductCategoryCellController.java
@@ -7,6 +7,8 @@ import se.chalmers.ait.dat215.project.ProductCategory;
 
 import java.util.List;
 
+import static se.chalmers.ait.dat215.project.ProductCategory.*;
+
 
 /**
  * Created by Johan on 2017-02-23.
@@ -23,7 +25,45 @@ public class ProductCategoryCellController extends AbstractCellController {
     }
 
     public void setLabels() {
-        nameLabel.setText("hej");
+            if (productCategoryList.get(0) == POD) {
+                nameLabel.setText("Alla produkter");
+            }
+
+            if (productCategoryList.get(0) == CITRUS_FRUIT) {
+                nameLabel.setText("Frukt och bär");
+            }
+
+            if (productCategoryList.get(0) == BREAD) {
+                nameLabel.setText("Bröd");
+            }
+
+            if (productCategoryList.get(0) == CABBAGE) {
+                nameLabel.setText("Grönsaker");
+            }
+
+            if (productCategoryList.get(0) == FISH) {
+                nameLabel.setText("Kött och fisk");
+            }
+
+            if (productCategoryList.get(0) == HOT_DRINKS) {
+                nameLabel.setText("Skafferi");
+            }
+
+            if (productCategoryList.get(0) == DAIRIES) {
+                nameLabel.setText("Mjölkprodukter");
+            }
+
+            if (productCategoryList.get(0) == COLD_DRINKS) {
+                nameLabel.setText("Drycker");
+            }
+
+            if (productCategoryList.get(0) == SWEET) {
+                nameLabel.setText("Godis");
+            }
+
+            if (productCategoryList.get(0) == PASTA) {
+                nameLabel.setText("Pasta/Ris/Potatis");
+            }
     }
 
     public void inject(List<ProductCategory> productCategoryList) {
