98698ad90a86f805c47e6ab5b48ba2a7a18556de
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-03-05 00:01:09 +0100

    added some more shit to the ConfirmationStage

--- src/iMat/controllers/stages/ConfirmationStageController.java
+++ src/iMat/controllers/stages/ConfirmationStageController.java
@@ -1,6 +1,15 @@
 package iMat.controllers.stages;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.ShoppingItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,8 +19,23 @@ import java.util.ResourceBundle;
  */
 public class ConfirmationStageController implements Initializable {
 
+    @FXML private Label presentationLabel;
+    @FXML private Button placeOrderButton;
+    @FXML private ListView<ShoppingItem> receiptListView;
+
+    IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        presentationLabel.setText("Hej " + dataHandler.getCustomer().getFirstName() + "!");
 
+        ObservableList<ShoppingItem> shoppingItemObservableList = FXCollections.observableArrayList();
+        shoppingItemObservableList.addAll(dataHandler.getShoppingCart().getItems());
+        receiptListView.setItems(shoppingItemObservableList);
+    }
+
+    @FXML
+    private void placeOrderActionPerformed(ActionEvent event) {
+        dataHandler.placeOrder(true);
     }
 }
