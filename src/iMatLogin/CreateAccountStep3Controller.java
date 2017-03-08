daaf499c78e62572d096e121b4d6072afe6342bc
knya <johsvenn@student.chalmers.se>
2017-03-08 10:14:56 +0100

    fixed some Buttons and added StackPane for Payment Options

--- src/iMatLogin/CreateAccountStep3Controller.java
+++ src/iMatLogin/CreateAccountStep3Controller.java
@@ -1,9 +1,16 @@
 package iMatLogin;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
 
 import javax.swing.*;
 import java.net.URL;
@@ -16,11 +23,31 @@ public class CreateAccountStep3Controller implements Initializable {
 
     private LoginController loginController;
 
+    @FXML private ComboBox<String> paymentComboBox;
     @FXML private Button createAccountButton;
+    @FXML private AnchorPane creditCardPane;
+    @FXML private AnchorPane invoicePane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> paymentList = FXCollections.observableArrayList(
+                "< Betalsätt >",
+                "Kreditkort",
+                "Faktura"
+        );
 
+        paymentComboBox.setItems(paymentList);
+        paymentComboBox.getSelectionModel().selectFirst();
+
+        paymentComboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == "Kreditkort") {
+                creditCardPane.toFront();
+            }
+
+            if (newValue == "Faktura") {
+                invoicePane.toFront();
+            }
+        });
     }
 
     public void inject(LoginController loginController) {
@@ -28,12 +55,12 @@ public class CreateAccountStep3Controller implements Initializable {
     }
 
     @FXML
-    protected void createAccountActionPerformed(ActionEvent event) {
+    private void createAccountActionPerformed(ActionEvent event) {
 
     }
 
     @FXML
-    protected void goBackwardActionPerformed(ActionEvent event) {
+    private void goBackwardActionPerformed(ActionEvent event) {
         loginController.getCreateAccountStep2().toFront();
     }
 }
