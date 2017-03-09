63285332d59e5bc7d67fe8b09054da51880fb4bf
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-03-09 23:17:25 +0100

    added some SamePageErrorMessages for the first step

--- src/iMatLogin/CreateAccountUserController.java
+++ src/iMatLogin/CreateAccountUserController.java
@@ -4,10 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -20,19 +23,29 @@ public class CreateAccountUserController implements Initializable {
     @FXML private TextField userNameField;
     @FXML private PasswordField passwordField;
     @FXML private PasswordField confirmPasswordField;
+    @FXML private Label userNameErrorLabel;
+    @FXML private Label passwordErrorLabel;
 
     @FXML private Button backToLoginButton;
     @FXML private Button goForwardButton;
 
+    private String confirmPassword = "";
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        clearErrorMessages();
+
         userNameField.textProperty().addListener((observable, oldValue, newValue) -> {
             loginController.getNewUser().setUserName(newValue);
         });
 
         passwordField.textProperty().addListener(((observable, oldValue, newValue) -> {
             loginController.getNewUser().setPassword(newValue);
+        }));
+
+        confirmPasswordField.textProperty().addListener(((observable, oldValue, newValue) -> {
+            confirmPassword = newValue;
         }));
     }
 
@@ -47,6 +60,55 @@ public class CreateAccountUserController implements Initializable {
 
     @FXML
     private void goForwardActionPerformed(ActionEvent event) {
-        loginController.getPersonalPane().toFront();
+        clearErrorMessages();
+
+        if (checkIfOnlyNumbers(userNameField) && checkPassword()) {
+            loginController.getPersonalPane().toFront();
+        } else {
+            setErrorLabels();
+        }
+    }
+
+    private void setErrorLabels() {
+        if (confirmPassword.isEmpty()) {
+            passwordErrorLabel.setText("Fält ej ifyllt.");
+        }
+
+        if (userNameField.getText().isEmpty()) {
+            userNameErrorLabel.setText("Fält ej ifyllt.");
+        }
+
+        if (!checkIfOnlyNumbers(userNameField)) {
+            userNameErrorLabel.setText("Personnumret får endast innehålla siffror.");
+        }
+
+        if (!checkPassword()) {
+            passwordErrorLabel.setText("Lösenordet matchar inte.");
+        }
+    }
+
+    private void clearErrorMessages() {
+        userNameErrorLabel.setText("");
+        passwordErrorLabel.setText("");
+    }
+
+    private boolean checkPassword() {
+        if (confirmPassword.isEmpty() && loginController.getNewUser().getPassword().isEmpty()) {
+            return false;
+        }
+        return confirmPassword.equals(loginController.getNewUser().getPassword());
+    }
+
+    private boolean checkIfOnlyNumbers(TextField phoneNumberField){
+        List<Character> numbers  = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            numbers.add((char)(i + '0'));
+        }
+        for(int i = 0; i < phoneNumberField.getText().length(); i++){
+            if(!numbers.contains(phoneNumberField.getText().charAt(i))){
+                return false;
+            }
+        }
+        return true;
     }
 }
