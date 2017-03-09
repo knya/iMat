0ab82169afdc2a68f6aeaa9908c6c40fb2d8c31b
knya <johsvenn@student.chalmers.se>
2017-03-09 10:35:18 +0100

    commented saving state when closing, program resets for now

--- src/iMat/Main.java
+++ src/iMat/Main.java
@@ -1,13 +1,18 @@
 package iMat;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
 
 
 public class Main extends Application {
+
+    private IMatDataHandler dataHandler = IMatDataHandler.getInstance();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -17,6 +22,9 @@ public class Main extends Application {
         primaryStage.setMinHeight(400);
         primaryStage.setMinWidth(362);
         primaryStage.show();
+
+//        primaryStage.setOnCloseRequest(event -> dataHandler.shutDown());
+        dataHandler.reset();
     }
 
 
