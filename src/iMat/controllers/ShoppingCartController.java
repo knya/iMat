1f305ea811e8669bae2f6ae26943836fefffc59a
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-03-10 01:00:49 +0100

    minor changes

--- src/iMat/controllers/ShoppingCartController.java
+++ src/iMat/controllers/ShoppingCartController.java
@@ -78,7 +78,7 @@ public class ShoppingCartController implements Initializable {
     @FXML
     private void goToCartActionPerformed(ActionEvent event) {
         mainController.getTabPane().getSelectionModel().select(2);
-        mainController.getTabController().getOrderTabController().backToCartActionPerformed(event);
+//        mainController.getTabController().getOrderTabController().backToCartActionPerformed(event);
     }
 
     @FXML
